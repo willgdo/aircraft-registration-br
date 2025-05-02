72e27924d1ddeb0d716f239f6bb08e168df0f903
Willian Oliveira <willian.oliveira737@gmail.com>
2025-05-01 22:21:35 -0300

    Add estrutura para styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./routes/AppRoutes.tsx";
-import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
